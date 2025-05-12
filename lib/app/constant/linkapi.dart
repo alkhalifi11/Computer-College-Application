8233de99de8e8d9ff689070c1170c38397106f44
Ahmed Alkhalif <ahmedalkhalifi31@gmail.com>
2025-05-12 20:12:48 +0300

    Update linkapi.dart

--- lib/app/constant/linkapi.dart
+++ lib/app/constant/linkapi.dart
@@ -1,5 +1,3 @@
-//const String linkServerName = "https://pixelatstudio.com/Univ";
-//const String linkimageRoot = "https://pixelatstudio.com/Univ/upload";
 const String linkServerName = "http://10.0.2.2/Univ";
 const String linkimageRoot = "http://10.0.2.2/Univ/upload";
 
